f627ec9606847362f5700bcc601c3728f774f8aa
Peewi <laursen.lau@gmail.com>
2022-12-10 12:24:49 +0100

    Day 10

---
+++ Day10a/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Day10a
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] lines = File.ReadAllLines("input.txt");
+			int x = 1;
+			int cycle = 0;
+			int signalSum = 0;
+			foreach (string line in lines)
+			{
+				cycle++;
+				if (Cycle(cycle, x))
+				{
+					signalSum += cycle * x;
+				}
+				string[] split = line.Split(' ');
+				if (split[0] == "addx")
+				{
+					cycle++;
+					if (Cycle(cycle, x))
+					{
+						signalSum += cycle * x;
+					}
+					x += int.Parse(split[1]);
+				}
+			}
+			Console.WriteLine($"Signal sum: {signalSum}");
+		}
+
+		static bool Cycle(int cycle, int x)
+		{
+			if ((cycle + 20) % 40 == 0)
+			{
+				Console.WriteLine($"Cycle {cycle} strength: {cycle * x}");
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ Day10b/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Day10b
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] lines = File.ReadAllLines("input.txt");
+			int x = 1;
+			int cycle = 0;
+			foreach (string line in lines)
+			{
+				cycle++;
+				Cycle(cycle, x);
+				string[] split = line.Split(' ');
+				if (split[0] == "addx")
+				{
+					cycle++;
+					Cycle(cycle, x);
+					x += int.Parse(split[1]);
+				}
+			}
+		}
+
+		static void Cycle(int cycle, int x)
+		{
+			int pos = (cycle - 1) % 40;
+			if (Math.Abs(pos - x) <= 1)
+			{
+				Console.Write("█");
+			}
+			else
+			{
+				Console.Write(" ");
+			}
+			if (cycle % 40 == 0)
+			{
+				Console.Write("\n");
+			}
+		}
+	}
+}
