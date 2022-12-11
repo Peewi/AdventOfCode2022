b0d1582fc9bd97c13a0a7112c11bbf4b45ec41a0
Peewi <laursen.lau@gmail.com>
2022-12-11 15:32:58 +0100

    Day 11 part 1

---
+++ Day11a/Monkey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11a
+{
+	enum MonkeyOperator
+	{
+		Add,
+		Multiply
+	}
+	internal class Monkey
+	{
+		public List<int> Items = new List<int>();
+		public int OperationAmount = 0;
+		public MonkeyOperator Operator = MonkeyOperator.Add;
+		public int DivisionTest = 0;
+		public int TrueTarget = 0;
+		public int FalseTarget = 0;
+		public int InspectionCount = 0;
+	}
+}
