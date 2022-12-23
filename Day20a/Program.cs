a11a06247466d4ecb3d07c6f6d1f4404f60fcdc9
Peewi <laursen.lau@gmail.com>
2022-12-23 12:02:23 +0100

    Days 20 and 21, part 1

---
+++ Day20a/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Day20a
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] input = File.ReadAllLines("input.txt");
+			List<int> numbers = new List<int>(input.Length);
+			List<int> locations = new List<int>(input.Length);
+			for (int i = 0; i < input.Length; i++)
+			{
+				string line = input[i];
+				numbers.Add(int.Parse(line));
+				locations.Add(i);
+			}
+
+			for (int i = 0; i < locations.Count; i++)
+			{
+				int index = locations[i];
+				int moveNumber = numbers[index];
+				int realNumber = numbers[index];
+				while (Math.Abs(moveNumber) >= numbers.Count)
+				{
+					moveNumber = Math.Sign(moveNumber) * (Math.Abs(moveNumber) - (numbers.Count - 1));
+				}
+				int newIndex = index + moveNumber;
+				newIndex = ((newIndex % numbers.Count) + numbers.Count) % numbers.Count;
+				
+				if (Math.Sign(moveNumber) != Math.Sign(newIndex - index))
+				{
+					newIndex -= Math.Sign(newIndex - index);
+				}
+				numbers.RemoveAt(index);
+				numbers.Insert(newIndex, realNumber);
+				// readjust locations
+				for (int j = i + 1; j < locations.Count; j++)
+				{
+
+					if (newIndex - index > 0 && locations[j] <= newIndex && locations[j] > index)
+					{
+						locations[j]--;
+					}
+					if (newIndex - index < 0 && locations[j] >= newIndex && locations[j] < index)
+					{
+						locations[j]++;
+					}
+
+					while (locations[j] < 0)
+					{
+						locations[j] += locations.Count;
+					}
+					locations[j] %= locations.Count;
+				}
+			}
+			Console.WriteLine("List mixed");
+			int zeroPos = numbers.IndexOf(0);
+			int[] important = new int[]
+			{
+				(zeroPos + 1000) % numbers.Count,
+				(zeroPos + 2000) % numbers.Count,
+				(zeroPos + 3000) % numbers.Count,
+			};
+			int importantSum = 0;
+			Console.WriteLine("Important numbers:");
+			foreach (int item in important)
+			{
+				Console.WriteLine(numbers[item]);
+				importantSum += numbers[item];
+			}
+			Console.WriteLine($"sum: {importantSum}");
+		}
+	}
+}
---
+++ Day21a/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Day21a
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, long> monkeyNumbers = new Dictionary<string, long>();
+			Dictionary<string, (string, string, string)> monkeyCalc = new Dictionary<string, (string, string, string)>();
+			string[] input = File.ReadAllLines("input.txt");
+			foreach (string line in input)
+			{
+				string[] split = line.Split(' ');
+				string name = split[0][0..4];
+				if (split.Length == 2)
+				{
+					monkeyNumbers.Add(name, int.Parse(split[1]));
+				}
+				else
+				{
+					monkeyCalc.Add(name, (split[1], split[2], split[3]));
+				}
+			}
+			//
+			const string IMPORTANTMONKEY = "root";
+			while (monkeyCalc.Count > 0)
+			{
+				List<string> foundMonkeys = new List<string>();
+				foreach (KeyValuePair<string, (string, string, string)> item in monkeyCalc)
+				{
+					if (monkeyNumbers.ContainsKey(item.Value.Item1) && monkeyNumbers.ContainsKey(item.Value.Item3))
+					{
+						long num1 = monkeyNumbers[item.Value.Item1];
+						long num2 = monkeyNumbers[item.Value.Item3];
+						long result = 0;
+						switch (item.Value.Item2)
+						{
+							case "+":
+								result = num1 + num2;
+								break;
+							case "-":
+								result = num1 - num2;
+								break;
+							case "*":
+								result = num1 * num2;
+								break;
+							case "/":
+								result = num1 / num2;
+								break;
+							default:
+								throw new Exception();
+						}
+						monkeyNumbers.Add(item.Key, result);
+						foundMonkeys.Add(item.Key);
+						if (item.Key == IMPORTANTMONKEY)
+						{
+							break;
+						}
+					}
+				}
+				if (monkeyNumbers.ContainsKey(IMPORTANTMONKEY))
+				{
+					break;
+				}
+				foreach (string item in foundMonkeys)
+				{
+					monkeyCalc.Remove(item);
+				}
+			}
+			Console.WriteLine($"{IMPORTANTMONKEY} will yell {monkeyNumbers[IMPORTANTMONKEY]}");
+		}
+	}
+}
